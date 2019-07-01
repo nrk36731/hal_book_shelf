08217dfb02385197f1d8d1184f296930a552530d
nrk36731 <52160188+nrk36731@users.noreply.github.com>
2019-07-01 21:37:05 +0530

    Add files via upload

--- db.js
+++ db.js
@@ -1,28 +1,29 @@
-var http = require('http');
+var  http=require('http');
 var fs=require('fs');
+var MongoClient=require('mongodb').MongoClient;
+var url="mongodb://localhost:27017/";
 var querystring=require('querystring');
-var  MongoClient=require('mongodb').MongoClient;
-var url="mongodb://localhost:27017/newdb";
 http.createServer(function(req,res){
-
-if(req.url=="/form"){
-res.writeHead(200,{"Content-Type":"text/html"});
-fs.createReadStream("./d.html","UTF-8").pipe(res);}
-
-if(req.method==="POST"){
-var data="";
-req.on("data",function(chunk){
-data+=chunk;
-});
-req.on("data",function(chunk){
- MongoClient.connect(url, function(err, result) {
-  if (err) throw err;
-var q=querystring.parse(data);
-db.collection('customer').insertOne(err,res){
-if (err) throw err;
+ if(req.url==="/form"){
+   res.writeHead(200,{"Content-Type":"text/html"});
+   fs.createReadStream("F:/Data/PROGRAM/HAL/Book Store/d.html","UTF-8").pipe(res);
+ }
+ if(req.method==="POST"){
+  var data=""; 
+  req.on("data",function(chunk){
+    data+=chunk;
+   });
+   req.on("end",function(chunk){
   
-db.close();
-});
-});
-}
+ MongoClient.connect(url,function(err,result){
+  if(err) throw err;
+  var q=querystring.parse(data);
+  db.collection('customer').insertOne(q,function(err,res){
+    if(err) throw err;
+    db.close();
+  });
+ });
+    console.log(q);
+   });
+ }
 }).listen(3000);
